0adf49a235e4036edd5ce31d5a81d9933f79dc59
Stefaan Lippens <soxofaan@gmail.com>
2020-08-10 12:24:16 +0200

    further finetuning of README

---
+++ examples/jupyter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Activate desired Python environment to run Jupyter.
+#####################################################
+# For example:
+# [A] with conda (environment activation is based on https://github.com/conda/conda/issues/7980)
+eval "$(conda shell.bash hook)"
+conda activate jupyter-env
+
+# [B] with standard venv/virtualenv
+# source path/to/venv/bin/activate
+
+# Launch Jupyter.
+#################
+jupyter notebook
+
+# Sleep a bit to allow capturing of error messages if launching Jupyter failed.
+sleep 5
